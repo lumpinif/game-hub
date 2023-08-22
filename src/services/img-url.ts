a97ade800addb7c179379071a0a9705175020c8a
LumPiniF <pengfeilyu1018@gmail.com>
2023-08-22 15:41:16 +0800

    handle game with no image

--- src/services/img-url.ts
+++ src/services/img-url.ts
@@ -1,5 +1,7 @@
+import noImage from '../assets/no-image-placeholder-6f3882e0.webp';
+
 const getCropedImageUrl = (url: string) => {
-  if (!url) return '';
+  if (!url) return noImage;
   const taget = 'media/';
   const index = url.indexOf(taget) + taget.length;
   return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
